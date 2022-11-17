1250cfbf5d8214d79f44eb3b29ca43a1bd5450ac
xiaohanlin101407 <69285512+xiaohanlin101407@users.noreply.github.com>
2022-11-17 17:09:20 +0800

    Update parser.js

--- parser.js
+++ parser.js
@@ -3,9 +3,9 @@ var translate;
 (function e(t, n, r) { function s(o, u) { if (!n[o]) { if (!t[o]) { var a = typeof require == "function" && require; if (!u && a) return a(o, !0); if (i) return i(o, !0); var f = new Error("Cannot find module '" + o + "'"); throw f.code = "MODULE_NOT_FOUND", f } var l = n[o] = { exports: {} }; t[o][0].call(l.exports, function (e) { var n = t[o][1][e]; return s(n ? n : e) }, l, l.exports, e, t, n, r) } return n[o].exports } var i = typeof require == "function" && require; for (var o = 0; o < r.length; o++)s(r[o]); return s })({
     1: [function (require, module, exports) {
             var md = require('markdown-it')(),
-            mk = require('./index',strict="error");
+            mk = require('./index');
 
-        md.use(mk,strict="error");
+        md.use(mk,{"strict":"error"});
 
         var input = document.getElementById('input'),
             output = document.getElementById('output'),
@@ -13,7 +13,7 @@ var translate;
 
         function s(input){
 
-            var result = md.render(input,strict="error");
+            var result = md.render(input);
 
             return result;
 
